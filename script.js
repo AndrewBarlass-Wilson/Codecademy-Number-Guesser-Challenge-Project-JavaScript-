6ea369f22b674b21930897b785f31c134682f2fa
AndrewBarlass-Wilson <84100147+AndrewBarlass-Wilson@users.noreply.github.com>
2021-05-12 21:11:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+function generateTarget() {
+    return Math.ceil(Math.random() * 10);
+}
+function compareGuesses(user, computer, target) {
+    if (user > 9 || user < 0) {alert('value out of range!')}
+    if (Math.abs(target - computer) < Math.abs(target - user)) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+function updateScore(winner) {
+    if (winner === 'human') { humanScore++; }
+    if (winner === 'computer') { computerScore++; }
+}
+
+function advanceRound() {
+    currentRoundNumber++;
+}
